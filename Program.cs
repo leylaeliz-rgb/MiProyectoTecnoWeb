97df308d02248d5cb8417f43d4d323080548eab5
Leyla <leyla.tambo@ucb.edu.bo>
2026-09-04 23:38:46 -0400

    feat: Definicion inicial de la clase Libro, con un metodo mostrar informacion

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+class Libro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Categoria { get; set; }
+    public string Codigo{get; set;}
+    public bool Disponibilidad { get; set; }
+
+    public Libro (string titulo, string autor, string categoria, string codigo, bool disponibilidad)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        Categoria = categoria;
+        Codigo = codigo;
+        disponibilidad = true;
+    }
+    
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"{Titulo} - Escrito por: {Autor} ({Categoria},{Codigo}), Estado: ");
+        if (Disponibilidad)
+            {
+            Console.WriteLine("Disponible");
+            }
+        else
+            {
+            Console.WriteLine("No disponible");
+            }
+    }
+}
